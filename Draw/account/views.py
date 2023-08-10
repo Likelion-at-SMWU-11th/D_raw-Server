b635434df6bb83eab0cc05c53c52d0e84b93ac9d
newoceanwave <leeseohyun02@naver.com>
2023-08-10 15:57:48 +0900

    [FIX] 카카오 로그인 에러 수정

--- Draw/account/views.py
+++ Draw/account/views.py
@@ -7,6 +7,48 @@ from rest_framework.permissions import AllowAny
 import requests
 
 
+def index(request):
+    _context = {'check':False}
+    if request.session.get('access_token'):
+        _context['check'] = True
+    return render(request, 'index.html', _context)
+
+def kakaoLoginLogic(request):
+    _restApiKey = 'd8fd6327b24b302b1d20f0690b10d3f4'
+    _redirectUrl = 'http://127.0.0.1:8000/account/kakaoLoginLogicRedirect'
+    _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
+    return redirect(_url)
+
+def kakaoLoginLogicRedirect(request):
+    _qs = request.GET['code']
+    _restApiKey = 'd8fd6327b24b302b1d20f0690b10d3f4'
+    _redirect_uri = 'http://127.0.0.1:8000/account/kakaoLoginLogicRedirect'
+    _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
+    _res = requests.post(_url)
+    _result = _res.json()
+    request.session['access_token'] = _result['access_token']
+    request.session.modified = True
+    return render(request, 'KakaoLogin.html')
+
+def kakaoLogout(request):
+    _token = request.session['access_token']
+    _url = 'https://kapi.kakao.com/v1/user/logout'
+    _header = {
+      'Authorization': f'bearer {_token}'
+    }
+    _url = 'https://kapi.kakao.com/v1/user/unlink'
+    _header = {
+       'Authorization': f'bearer {_token}',
+     }
+    _res = requests.post(_url, headers=_header)
+    _result = _res.json()
+    if _result.get('id'):
+        del request.session['access_token']
+        return render(request, 'KakaoLogoutSuccess.html')
+    else:
+        return render(request, 'KakaoLogoutError.html')
+
+'''
 @api_view(['GET'])
 @permission_classes([AllowAny, ])
 def kakaoGetLogin(request):
@@ -55,3 +97,4 @@ class KakaoSignup(View):
         client_id = 'd8fd6327b24b302b1d20f0690b10d3f4'
 
         return redirect(f"{kakao_api}&client_id={client_id}&redirect_url={redirect_url}") 
+        '''
