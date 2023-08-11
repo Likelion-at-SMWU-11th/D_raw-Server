0ca0dd242eeaf1893996a088286fa2dd5c29789e
newoceanwave <leeseohyun02@naver.com>
2023-08-11 14:39:16 +0900

    [FIX] 카카오 로그인 구현

--- Draw/account/views.py
+++ Draw/account/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect
-from django.views import View
-import os
-from Draw.settings import SOCIAL_OUTH_CONFIG
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
 import requests
+import json
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
 
 
 def index(request):
@@ -14,21 +12,21 @@ def index(request):
     return render(request, 'index.html', _context)
 
 def kakaoLoginLogic(request):
-    _restApiKey = 'd8fd6327b24b302b1d20f0690b10d3f4'
-    _redirectUrl = 'http://127.0.0.1:8000/account/kakaoLoginLogicRedirect'
+    _restApiKey = 'd8fd6327b24b302b1d20f0690b10d3f4' # 입력필요
+    _redirectUrl = 'http://127.0.0.1:8000/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/authorize?client_id={_restApiKey}&redirect_uri={_redirectUrl}&response_type=code'
     return redirect(_url)
 
 def kakaoLoginLogicRedirect(request):
     _qs = request.GET['code']
-    _restApiKey = 'd8fd6327b24b302b1d20f0690b10d3f4'
-    _redirect_uri = 'http://127.0.0.1:8000/account/kakaoLoginLogicRedirect'
+    _restApiKey = 'd8fd6327b24b302b1d20f0690b10d3f4' # 입력필요
+    _redirect_uri = 'http://127.0.0.1:8000/kakaoLoginLogicRedirect'
     _url = f'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={_restApiKey}&redirect_uri={_redirect_uri}&code={_qs}'
     _res = requests.post(_url)
     _result = _res.json()
     request.session['access_token'] = _result['access_token']
     request.session.modified = True
-    return render(request, 'KakaoLogin.html')
+    return render(request, 'loginSuccess.html')
 
 def kakaoLogout(request):
     _token = request.session['access_token']
@@ -36,65 +34,28 @@ def kakaoLogout(request):
     _header = {
       'Authorization': f'bearer {_token}'
     }
-    _url = 'https://kapi.kakao.com/v1/user/unlink'
-    _header = {
-       'Authorization': f'bearer {_token}',
-     }
+    # _url = 'https://kapi.kakao.com/v1/user/unlink'
+    # _header = {
+    #   'Authorization': f'bearer {_token}',
+    # }
     _res = requests.post(_url, headers=_header)
     _result = _res.json()
     if _result.get('id'):
         del request.session['access_token']
-        return render(request, 'KakaoLogoutSuccess.html')
+        return render(request, 'loginoutSuccess.html')
     else:
-        return render(request, 'KakaoLogoutError.html')
+        return render(request, 'logoutError.html')
 
-'''
-@api_view(['GET'])
-@permission_classes([AllowAny, ])
-def kakaoGetLogin(request):
-    CLIENT_ID = SOCIAL_OUTH_CONFIG['KAKAO_REST_API_KEY']
-    REDIRET_URL = SOCIAL_OUTH_CONFIG['KAKAO_REDIRECT_URI']
-    url = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={0}&redirect_uri={1}".format(
-        CLIENT_ID, REDIRET_URL)
-    res = redirect(url)
-    return res
-
-@api_view(['GET'])
-@permission_classes([AllowAny, ])
-def getUserInfo(reqeust):
-    CODE = reqeust.query_params['code']
-    url = "https://kauth.kakao.com/oauth/token"
-    res = {
-            'grant_type': 'authorization_code',
-            'client_id': SOCIAL_OUTH_CONFIG['KAKAO_REST_API_KEY'],
-            'redirect_url': SOCIAL_OUTH_CONFIG['KAKAO_REDIRECT_URI'],
-            'client_secret': SOCIAL_OUTH_CONFIG['KAKAO_SECRET_KEY'],
-            'code': CODE
-        }
-    headers = {
-        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
-    }
-    response = requests.post(url, data=res, headers=headers)
-    # 그 이후 부분
-    tokenJson = response.json()
-    userUrl = "https://kapi.kakao.com/v2/user/me" # 유저 정보 조회하는 uri
-    auth = "Bearer "+tokenJson['access_token'] ## 'Bearer '여기에서 띄어쓰기 필수!!
-    HEADER = {
-        "Authorization": auth,
-        "Content-type": "application/x-www-form-urlencoded;charset=utf-8"
-    }
-    res = requests.get(userUrl, headers=HEADER)
-    return response(res.text)
-
-def account(request):
-    return render(request, 'signup.html')
-
-
-class KakaoSignup(View):
-    def get(self, request):
-        kakao_api = "https://kauth.kakao.com/oauth/authorize?response_type=code"
-        redirect_url = "http://127.0.0.1:8000/account/kakao/login/callback"
-        client_id = 'd8fd6327b24b302b1d20f0690b10d3f4'
-
-        return redirect(f"{kakao_api}&client_id={client_id}&redirect_url={redirect_url}") 
-        '''
+def methodsCheck(request, id):
+    if(request.method == 'GET'):
+        print(f"GET QS : {request.GET.get('data', '')}")
+        print(f"GET Dynamic Path : {id}")
+    
+    # PostMan으로 Localhost 테스트를 위해 CSRF 해제
+    # project/settings.py 파일에서 
+    # MIDDLEWARE -> 'django.middleware.csrf.CsrfViewMiddleware' 주석 처리
+    elif(request.method == 'POST'):
+        print(f"POST QS : {request.GET.get('data', '')}")
+        print(f"POST Dynamic Path : {id}")
+        return HttpResponse("POST Request.", content_type="text/plain")
+    return render(request, 'methodGet.html')
