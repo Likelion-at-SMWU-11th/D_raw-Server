e4936fb657bd531e56c2a39a9d5e896686475167
윤서희 <appleid21@sookmyung.ac.kr>
2023-08-13 03:52:24 +0900

    [Feat] 매칭화면 마무리

--- Draw/match/urls.py
+++ Draw/match/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('', views.match, name='match'),
+    path('check/', views.check, name='check'),
+    path('quick/', views.quick, name='quick'),
+    path('profile/', views.profile, name='profile'),
 ]
